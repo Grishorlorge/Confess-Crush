2c53db78296eed84f2a8f0c34009ddd7afe9094d
Grishorlorge <108349566+Grishorlorge@users.noreply.github.com>
2022-06-28 21:48:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    titleWeb: "Ai biết gì đâu",
+    introTitle: 'Bé iu của anh',
+    introDesc: `Nhớ anh hong ???`,
+    btnIntro: 'Cóaaaa',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    desc: 'Sau 1 tháng còn muốn chơi zới anh hong :)) ',
+    btnYes: 'Còn nha :33',
+    btnNo: 'Hong nha , bái baii :))',
+    question: 'Sao em lại iu anh ó :))',
+    btnReply: 'Gửi cho anhhh',
+    reply: 'Tại anh dễ thương :)))',
+    mess: 'Anh biết mà :)). Yêu bé nhiều lắm lun <3',
+    messDesc: 'Ù sau 1 tháng anh chỉ thấy anh ngày càng iu em nhìu hơn hoi. Hóng tới lúc đi học lại được gặp bé iu quóoo <3',
+    btnAccept: 'Iu anh <3',
+    messLink: 'https://Gris/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
